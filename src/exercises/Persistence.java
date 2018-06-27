db8c5305df12d874f74c92ecf570ed15e4d7c91c
DavidCorral94 <david.corral@uca.es>
2018-06-27 12:24:56 +0200

    Minor change

--- src/exercises/Persistence.java
+++ src/exercises/Persistence.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
 public class Persistence {
 
     public static void main(String[] args) {
-        //testMySQL();ç
+        //testMySQL();
         testMongoDB();
     }
 
